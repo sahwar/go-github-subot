16d6f621abe4f88c0629d5e4fbb48b9a50dfd7d5
Дмитрий <Dmitriy.Vas.B@gmail.com>
2019-04-09 12:21:03 +0500

    Add test

---
+++ subot_test.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"encoding/json"
+	"testing"
+)
+
+func TestSubot(t *testing.T) {
+	// Sets username to send as User-Agent
+	config.UserName = "Subot"
+	var users []User
+
+	// Get response from https://api.github.com/users
+	data, status := get(API+"/users", nil)
+	// Attempt to parse response as array of User structure
+	if err := json.Unmarshal(data, &users); err != nil || status != 200 {
+		t.Fatal(err, status)
+	}
+
+	// The first user. Should be mojombo
+	mojombo := users[0]
+
+	// Gives an error message if user isn't mojombo
+	if mojombo.Id != 1 || mojombo.Login != "mojombo" {
+		t.Fatal(mojombo)
+	}
+}
